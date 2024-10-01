b0b9b14cf298aed2d7e5e0d9948606089a9ed7e0
Chris Rayner <crayner@spartaglobal.com>
2024-10-01 22:43:56 +0100

    added small line for manipulating date

--- init/index.php
+++ init/index.php
@@ -1,5 +1,6 @@
 <?php
-$name = 'Chris'
+$name = 'Chris';
+$output = date('d-m-Y h:i:s a');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,6 +21,10 @@ $name = 'Chris'
     <div id="hellotext" class = h1>
         <?php echo 'Hello '.$name.'!';?>
     </div>
+</div>
+
+<div>
+    <?php echo 'Time & date: '.$output?>
 </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
